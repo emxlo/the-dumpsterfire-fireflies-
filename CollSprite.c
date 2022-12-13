a4d9197f4f221b68d8c6bd3c04203d702e31c271
emxlo <119593265+emxlo@users.noreply.github.com>
2022-12-13 13:26:27 +0100

    Add files via upload

---
+++ CollSprite.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <ncurses.h>
+int main() {
+ int maxy = 0, maxx = 0;
+ int newx = 0, newy = 0;
+ int direzionex = 1, direzioney = 1;
+
+initscr();
+noecho();
+curs_set(false);
+char sprite[] = {"<-||->"};
+int spritelen = sizeof(sprite)/sizeof(sprite[0]) - 1;
+getmaxyx(stdscr, maxy, maxx); /* Ottiene dimensioni schermo */
+int x = maxx/2, y = maxy/2; /* Centro dello schermo */
+
+ while(true) {
+clear();
+mvprintw(y, x, sprite);
+refresh();
+usleep(30000);
+newx = x + direzionex;
+newy = y + direzioney;
+/* Inverte la direzione del movimento toccando il bordo */
+ if(newx + spritelen >= maxx || newx < 0) direzionex*= -1;
+ else x+= direzionex;
+
+ if(newy >= maxy || newy < 0) 
+    direzioney*= - 1;
+else y+= direzioney;
+
+timeout(1); /* Non attende il tasto invio */
+if(getch() == 113) break; /* Esce alla pressione del tasto 'q' */
+}
+endwin();}
+
